c2454046aea7df4f0f7de705fff11f3be800c63a
KomalBharadva <55272896+KomalBharadva@users.noreply.github.com>
2020-06-11 16:05:13 +0530

    Add files via upload

---
+++ dsmp-pre-work-master/Manipulating-data-with-numpy.py
@@ -0,0 +1,95 @@
+# --------------
+# Importing header files
+import numpy as np
+
+# Path of the file has been stored in variable called 'path'
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+print("\nData: \n\n", data)
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+new=np.array(new_record)
+census=np.concatenate([data,new])
+#Code starts here
+
+
+
+# --------------
+#Code starts here
+age=np.array([census[:,0]],dtype=np.intp)
+print(age)
+max_age=np.amax(age)
+min_age=np.amin(age)
+age_mean=np.mean(age)
+age_std=np.std(age)
+print(max_age)
+print(min_age)
+print(age_mean)
+print(age_std)
+
+
+
+
+# --------------
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+race_00=np.where(census[:,2]==0)
+race_01=np.where(census[:,2]==1)
+race_02=np.where(census[:,2]==2)
+race_03=np.where(census[:,2]==3)
+race_04=np.where(census[:,2]==4)
+race_000=np.asarray(race_00)
+race_001=np.asarray(race_01)
+race_002=np.asarray(race_02)
+race_003=np.asarray(race_03)
+race_004=np.asarray(race_04)
+len_0=race_000.size
+len_1=race_001.size
+len_2=race_002.size
+len_3=race_003.size
+len_4=race_004.size
+print(len_0)
+#len_1=race_01.size
+#len_2=race_02.size
+#len_3=race_03.size
+#len_4=race_04.size
+minimum=min(len_0,len_1,len_2,len_3,len_4)
+print(minimum)
+minority_race=3
+print(minority_race)
+
+
+
+# --------------
+#Code starts here
+senior_citizens1=census[census[:,0]>60]
+senior_citizens=senior_citizens1.astype('int32')
+working_hours=senior_citizens1[:,6]
+print(working_hours)
+working_hours_sum=sum(working_hours)
+print(working_hours_sum)
+senior_citizens_len=len(senior_citizens)
+print(senior_citizens_len)
+avg_working_hours=working_hours_sum/senior_citizens_len
+print(avg_working_hours)
+
+
+
+# --------------
+#Code starts here
+high=census[census[:,1]>10]
+low=census[census[:,1]<=10]
+print(high)
+print(low)
+avg_pay_high=np.mean(high[:,7])
+avg_pay_low=np.mean(low[:,7])
+print(avg_pay_high)
+print(avg_pay_low)
+
+
+
+
+
+
---
+++ dsmp-pre-work-master/Visualization.py
@@ -0,0 +1,66 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#Code starts here
+data=pd.read_csv(path)
+loan_status=data['Loan_Status'].value_counts()
+loan_status.plot(kind='bar', stacked=True, figsize=(15,10))
+# Display plot
+plt.show()
+
+
+
+
+
+
+# --------------
+#Code starts here
+property_and_loan=data.groupby(['Property_Area','Loan_Status']).size().unstack()
+property_and_loan.plot(kind='bar', stacked=False, figsize=(15,10))
+plt.xlabel('Property Area')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+# Display plot
+plt.show()
+
+
+# --------------
+#Code starts here
+education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked=True,figsize=(15,10))
+property_and_loan.plot(kind='bar', stacked=False, figsize=(15,10))
+plt.xlabel('Education Status')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+# Display plot
+plt.show()
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education']=='Graduate']
+not_graduate=data[data['Education']=='Not Graduate']
+graduate.plot(kind='density', stacked=True, figsize=(15,10))
+plt.xlabel('Graduate')
+not_graduate.plot(kind='density', stacked=True, figsize=(15,10))
+plt.xlabel('Not Graduate')
+#Code ends here
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig ,(ax_1,ax_2,ax_3) = plt.subplots(nrows=3 ,ncols=1)
+ax_1.scatter(x='ApplicantIncome',y='LoanAmount')
+ax_1.set_title('Applicant Income')
+ax_2.scatter(x='CoapplicantIncome',y='LoanAmount')
+ax_2.set_title('Coapplicant Income')
+TotalIncome=data['ApplicantIncome'] + data['CoapplicantIncome']
+data['TotalIncome'] = TotalIncome
+ax_3.scatter(x='TotalIncome',y='LoanAmount')
+ax_3.set_title('Total Income')
+
+
